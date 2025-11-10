a2b0f8f3e274e6b2ad26bcb990ea39a7268da526
mennamohamed0207 <menamohamed0207@gmail.com>
2025-11-10 06:51:48 +0200

    adding first post request

---
+++ src/main/java/PostRequestGetAuthBooking.java
@@ -0,0 +1,52 @@
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import io.restassured.response.ValidatableResponse;
+import io.restassured.specification.RequestSpecification;
+import org.hamcrest.Matcher;
+import org.hamcrest.Matchers;
+import org.testng.annotations.Test;
+
+public class PostRequestGetAuthBooking {
+    /*
+    *
+{
+    "username" : "admin",
+    "password" : "password123"
+}
+* */
+    String jsonString = "{\"username\" : \"admin\",\"password\" : \"password123\"}";
+
+    @Test
+    public void postAuth_NonBDD() {
+        /*
+        We can pass body as a String or a JSON file or a XML file or a Java Object or a byte array.
+         * */
+        RequestSpecification request = RestAssured.given();
+        request.body(jsonString);
+        request.contentType(ContentType.JSON);
+        //Adding URI
+        request.baseUri("https://restful-booker.herokuapp.com/auth");
+        Response response = request.post();
+        System.out.println(response.asString());
+
+
+        ValidatableResponse validatableResponse = response.then();
+
+        validatableResponse.statusCode(200);
+        validatableResponse.body("token", Matchers.notNullValue());
+        validatableResponse.body("token.length()", Matchers.is(15));
+
+
+    }
+
+    @Test
+    public void postAuth_BDD() {
+        RestAssured.given().baseUri("https://restful-booker.herokuapp.com/auth").body(jsonString).contentType(ContentType.JSON)
+                .when()
+                .post()
+                .then()
+                .assertThat()
+                .body("token", Matchers.notNullValue());
+    }
+}
